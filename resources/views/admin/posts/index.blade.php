5c94bea5636b0be94def97315f863ba28400393b
andrea-prina <and.prina@gmail.com>
2022-09-19 17:58:30 +0200

    Create posts index view

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Author</th>
+                <th scope="col">Title</th>
+                <th scope="col">Content</th>
+                <th scope="col">Date</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($posts as $post)
+    
+                <tr>
+                    <td>
+                        {{ $post['id'] }}
+                    </td>
+                    <td>
+                        {{ $post['author'] }}
+                    </td>
+                    <td>
+                        {{ $post['title'] }}
+                    </td>
+                    <td>
+                        {{ $post['post_content'] }}
+                    </td>
+                    <td>
+                        {{ $post['post_date'] }}
+                    </td>
+                </tr>
+    
+            @empty
+    
+                <tr>
+                    <td colspan="5">Empty</td>
+                </tr>
+    
+            @endforelse
+        </tbody>
+    </table>
+</div>
+
+@endsection
