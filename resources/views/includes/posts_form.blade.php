489f695ecd158d6a08d4a44ca42ddad66341985c
andrea-prina <and.prina@gmail.com>
2022-09-20 12:15:52 +0200

    Create posts.create with reusable form

---
+++ resources/views/includes/posts_form.blade.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="row my-3">
+        <div class="col-12">
+            <label for="title">Post title</label>
+            <input type="text" id="title" class="form-control" name="title">
+        </div>
+    </div>
+    <div class="row my-3">
+        <div class="col-12">
+            <label for="post_content">Post content</label>
+            <textarea name="post_content" id="post_content" cols="30" rows="10" class="form-control"></textarea>
+        </div>
+    </div>
+    <div class="row my-3">
+        <div class="col-12">
+            <label for="post_image">Post image url</label>
+            <input type="text" id="post_image" class="form-control" name="post_image">
+        </div>
+    </div>
+    <input type="submit" value="Save & Publish" class="btn btn-primary">
+</div>
